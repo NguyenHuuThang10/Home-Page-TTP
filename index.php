c7ab93027dec33bc712563dd4cefd07d6acd32e0
NguyenHuuThang 10 <nguyenhuuthangag123@gmail.com>
2025-03-11 11:54:40 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'>
+    <link rel="stylesheet" href="./font/fontawesome-free-6.7.2-web/css/all.css">
+    <link rel="stylesheet" href="./css/style.css?ver=<?php echo rand() ?>">
+</head>
+
+<body>
+    <div class="main">
+
+        <div id="header">
+            <div class="head-icon">
+                <img src="./img/LOGO TTP_2024-04 2.svg" alt="">
+            </div>
+
+            <div class="head-nav">
+                <ul class="nav">
+                    <li class="nav-item">
+                        <a class="" href="#">VỀ CHÚNG TÔI</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="" href="#">THƯƠNG HIỆU</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="" href="#">SẢN PHẨM</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="" href="#">TIN TỨC</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="" href="#">TUYỂN DỤNG</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="" href="#">LIÊN HỆ</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="head-right">
+                <form class="form-search">
+                    <input type="text" name="" placeholder="Tìm kiếm...">
+                    <button type="submit"><i class="fa fa-search"
+                            aria-hidden="true"></i></button>
+                </form>
+
+                <div class="head-shopping">
+                    <img src="./img/shopping-cart.svg" alt="">
+                </div>
+            </div>
+
+        </div>
+
+        <div id="slider">
+            <div class="slider-btn">
+                <a href="#" class="btn-mua">MUA NGAY</a>
+                <a href="#" class="btn-mua">THÊM VÀO GIỎ HÀNG</a>
+            </div>
+        </div>
+
+        <div id="content">
+            <div id="about" class="about-section">
+                <div class="row">
+                    <div class="col-5">
+                        <h1 class="title-head text-orange mt45">VỀ CHÚNG TÔI</h1>
+
+                        <p class="about-des">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur blandit eleifend tellus, et efficitur nulla consectetur vitae. Donec facilisis lorem sit amet scelerisque maximus. Sed sed sollicitudin elit. Nulla nec congue nisi. Nam nec blandit neque, sit amet sollicitudin velit. Ut condimentum ex vitae sapien porta interdum id in nulla. Sed sollicitudin suscipit est accumsan pharetra. Integer ut suscipit sem. Phasellus volutpat consequat lectus, ornare porttitor justo volutpat sit amet.</p>
+
+                        <a href="#" class="read-more">Đọc tiếp</a>
+                    </div>
+
+                    <div class="col-7 about-img">
+                        <img src="./img/Group 2.png" alt="">
+                    </div>
+                </div>
+            </div>
+
+
+            <div id="vision">
+                <div class="content-section">
+                    <h1 class="title-head text-white text-center">TẦM NHÌN - SỨ MỆNH - GIÁ TRỊ</h1>
+
+                    <div class="vision-list">
+                        <div class="row">
+                            <div class="col-6 box-gm">
+                                <div class="vision-img">
+                                    <img src="./img/vision 1.svg" alt="">
+                                </div>
+                                <p class="vision-body text-white">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur blandit eleifend tellus, et efficitur nulla consectetur vitae. Donec facilisis lorem sit amet scelerisque maximus. Sed sed sollicitudin elit. Nulla nec congue nisi. Nam nec blandit neque, sit amet sollicitudin velit. Ut condimentum ex vitae.
+                                </p>
+                            </div>
+
+                            <div class="col-6 box-gm">
+                                <div class="vision-img">
+                                    <img src="./img/vision 2.svg" alt="">
+                                </div>
+                                <p class="vision-body text-white">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur blandit eleifend tellus, et efficitur nulla consectetur vitae. Donec facilisis lorem sit amet scelerisque maximus. Sed sed sollicitudin elit. Nulla nec congue nisi. Nam nec blandit neque, sit amet sollicitudin velit. Ut condimentum ex vitae.
+                                </p>
+                            </div>
+
+                            <div class="col-6 box-gm">
+                                <div class="vision-img">
+                                    <img src="./img/vision 3.svg" alt="">
+                                </div>
+                                <p class="vision-body text-white">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur blandit eleifend tellus, et efficitur nulla consectetur vitae. Donec facilisis lorem sit amet scelerisque maximus. Sed sed sollicitudin elit. Nulla nec congue nisi. Nam nec blandit neque, sit amet sollicitudin velit. Ut condimentum ex vitae.
+                                </p>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div id="business" class="content-section">
+                <h1 class="title-head text-orange text-center">HOẠT ĐỘNG KINH DOANH</h1>
+
+                <p class="business-des text-center mt30">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
+                <div class="business-list">
+                    <div class="business-item">
+                        <div class="row">
+                            <div class="col-6 text-center">
+                                <img src="./img/01.png" alt="" class="business-img-01">
+                            </div>
+                            <div class="col-6 d-flex align-items-center">
+                                <div class="business-text ">
+                                    <h1 class="business-head">
+                                        Tinh chế Yến sào - Yến thô
+                                    </h1>
+
+                                    <div class="rectangle mt30"></div>
+
+                                    <p class="business-content mt30">
+                                        Donec vestibulum vestibulum eleifend. Mauris pharetra purus nunc, sed hendrerit nisi laoreet id. Mauris vel gravida neque. Fusce aliquet velit et eros cursus, varius pretium magna sollicitudin. Morbi magna nisl, scelerisque sit amet sem in, placerat laoreet nisl. Duis dui dolor, pulvinar nec erat non, porta gravida ipsum. Morbi scelerisque, ante quis volutpat dapibus, erat orci ultricies sem, ac aliquet lorem lacus pretium felis. Nunc faucibus nulla ac ante aliquet, ac tempus ex aliquet. Vestibulum egestas, libero ullamcorper tempor porta, lorem mi aliquet ante, eget eleifend dolor ligula in orci. Vivamus erat ante, placerat eget nisl at, hendrerit tristique turpis. Proin non faucibus nulla. Aliquam erat volutpat. Proin ante eros, tincidunt nec dignissim vehicula, convallis vitae lorem.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="business-item">
+                        <div class="row">
+                            <div class="col-6 d-flex align-items-center">
+                                <div class="business-text ">
+                                    <h1 class="business-head">
+                                        Thực phẩm đóng họp
+                                    </h1>
+
+                                    <div class="rectangle mt30"></div>
+
+                                    <p class="business-content mt30">
+                                        Donec vestibulum vestibulum eleifend. Mauris pharetra purus nunc, sed hendrerit nisi laoreet id. Mauris vel gravida neque. Fusce aliquet velit et eros cursus, varius pretium magna sollicitudin. Morbi magna nisl, scelerisque sit amet sem in, placerat laoreet nisl. Duis dui dolor, pulvinar nec erat non, porta gravida ipsum. Morbi scelerisque, ante quis volutpat dapibus, erat orci ultricies sem, ac aliquet lorem lacus pretium felis. Nunc faucibus nulla ac ante aliquet, ac tempus ex aliquet. Vestibulum egestas, libero ullamcorper tempor porta, lorem mi aliquet ante, eget eleifend dolor ligula in orci. Vivamus erat ante, placerat eget nisl at, hendrerit tristique turpis. Proin non faucibus nulla. Aliquam erat volutpat. Proin ante eros, tincidunt nec dignissim vehicula, convallis vitae lorem.
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div class="col-6 text-center">
+                                <img src="./img/02.png" alt="" class="business-img">
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div class="business-item">
+                        <div class="row">
+                            <div class="col-6 text-center">
+                                <img src="./img/03.png" alt="" class="business-img">
+                            </div>
+                            <div class="col-6 d-flex align-items-center">
+                                <div class="business-text ">
+                                    <h1 class="business-head">
+                                        Sản xuất gạo
+                                    </h1>
+
+                                    <div class="rectangle mt30"></div>
+
+                                    <p class="business-content mt30">
+                                        Donec vestibulum vestibulum eleifend. Mauris pharetra purus nunc, sed hendrerit nisi laoreet id. Mauris vel gravida neque. Fusce aliquet velit et eros cursus, varius pretium magna sollicitudin. Morbi magna nisl, scelerisque sit amet sem in, placerat laoreet nisl. Duis dui dolor, pulvinar nec erat non, porta gravida ipsum. Morbi scelerisque, ante quis volutpat dapibus, erat orci ultricies sem, ac aliquet lorem lacus pretium felis. Nunc faucibus nulla ac ante aliquet, ac tempus ex aliquet. Vestibulum egestas, libero ullamcorper tempor porta, lorem mi aliquet ante, eget eleifend dolor ligula in orci. Vivamus erat ante, placerat eget nisl at, hendrerit tristique turpis. Proin non faucibus nulla. Aliquam erat volutpat. Proin ante eros, tincidunt nec dignissim vehicula, convallis vitae lorem.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="business-item">
+                        <div class="row">
+                            <div class="col-6 d-flex align-items-center">
+                                <div class="business-text ">
+                                    <h1 class="business-head">
+                                        Nước giải khát
+                                    </h1>
+
+                                    <div class="rectangle mt30"></div>
+
+                                    <p class="business-content mt30">
+                                        Donec vestibulum vestibulum eleifend. Mauris pharetra purus nunc, sed hendrerit nisi laoreet id. Mauris vel gravida neque. Fusce aliquet velit et eros cursus, varius pretium magna sollicitudin. Morbi magna nisl, scelerisque sit amet sem in, placerat laoreet nisl. Duis dui dolor, pulvinar nec erat non, porta gravida ipsum. Morbi scelerisque, ante quis volutpat dapibus, erat orci ultricies sem, ac aliquet lorem lacus pretium felis. Nunc faucibus nulla ac ante aliquet, ac tempus ex aliquet. Vestibulum egestas, libero ullamcorper tempor porta, lorem mi aliquet ante, eget eleifend dolor ligula in orci. Vivamus erat ante, placerat eget nisl at, hendrerit tristique turpis. Proin non faucibus nulla. Aliquam erat volutpat. Proin ante eros, tincidunt nec dignissim vehicula, convallis vitae lorem.
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div class="col-6 text-center">
+                                <img src="./img/04.png" alt="" class="business-img">
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+        <div id="footer">
+
+        </div>
+    </div>
+
+</body>
+<script src="./js/main.js"></script>
+<script src="./font/fontawesome-free-6.7.2-web/js/all.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+
+</html>
